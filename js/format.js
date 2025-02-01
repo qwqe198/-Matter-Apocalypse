485d98ba7a8810444c42970aa5cda99779cffaea
qwqe198 <146399264+qwqe198@users.noreply.github.com>
2025-02-01 18:29:28 +0800

    Update format.js

--- js/format.js
+++ js/format.js
@@ -231,7 +231,7 @@ const FORMATS = {
         }
     },
     layer: {
-      layers: ["无限", "永恒", "现实", "平等", "亲和", "迅捷", "同一", 活力", "免疫", "暴行"],
+      layers: [["infinity","eternity","reality","equality","affinity","celerity","identity","vitality","immunity","atrocity"],
       format(ex, acc, max) {
         ex = E(ex)
         let layer = ex.max(1).log10().max(1).log(INFINITY_NUM.log10()).floor()
